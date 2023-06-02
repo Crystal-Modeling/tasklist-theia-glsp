903c5bc6df73261c927c562b2221e71a57e00a16
Gregory Popov <tordektopor@gmail.com>
2023-06-02 14:36:21 +0000

    Correct Get Model API path and synchronize with tasklist-lang (add SSL to LMS client)

--- glsp-server/src/handler/lms-request-model-action-handler.ts
+++ glsp-server/src/handler/lms-request-model-action-handler.ts
@@ -1,6 +1,8 @@
 import { Action, ActionHandler, Logger, RequestModelAction, RequestModelActionHandler } from '@eclipse-glsp/server-node';
+import * as fs from 'fs';
 import * as http2 from 'http2';
 import { inject, injectable } from 'inversify';
+import * as path from 'path';
 
 @injectable()
 export class LmsRequestModelActionHandler extends RequestModelActionHandler implements ActionHandler {
@@ -14,9 +16,15 @@ export class LmsRequestModelActionHandler extends RequestModelActionHandler impl
         this.notifyClient('Model loading in progress');
         await this.sourceModelStorage.loadSourceModel(action);
         this.log.info('!!!! REQUESTING MODEL FROM LMS ....');
-        const session = http2.connect('http://localhost:8080');
+        const certificateAuthority = fs.readFileSync(path.join(__dirname, '../lms-ssl/cert.pem'));
+        const session = http2.connect('https://localhost:8443', {
+            // we don't have to do this if our certificate is signed by
+            // a recognized certificate authority, like LetsEncrypt
+            ca: certificateAuthority
+        });
+        const { HTTP2_HEADER_PATH } = http2.constants;
         session.on('error', this.log.error);
-        const request = session.request({ path: '/' });
+        const request = session.request({ [HTTP2_HEADER_PATH]: '/models/a5b691ce-5902-4d5f-bb6f-7ec31f9ad595' });
         request.end();
         request.setEncoding('utf8');
         let data = '';
