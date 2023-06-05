c818ad3c8b2efbe4422521dd2e3b9de87eeaba45
Gregory Popov <tordektopor@gmail.com>
2023-06-05 17:02:02 +0000

    Replace semantically meaningful data loaded from JSON with data from LMS

--- glsp-server/src/model/lms-tasklist-storage.ts
+++ glsp-server/src/model/lms-tasklist-storage.ts
@@ -8,6 +8,7 @@ import {
 } from '@eclipse-glsp/server-node';
 import { inject, injectable } from 'inversify';
 import { TaskListLmsClient } from '../lms/client/tasklist-lms-client';
+import * as src from '../lms/model';
 import { TaskList } from './tasklist-model';
 import { TaskListModelState } from './tasklist-model-state';
 
@@ -19,25 +20,20 @@ export class TaskListStorage extends AbstractJsonModelStorage {
     @inject(TaskListModelState)
     protected override modelState: TaskListModelState;
 
-    loadSourceModel(action: RequestModelAction): MaybePromise<void> {
+    public override async loadSourceModel(action: RequestModelAction): Promise<void> {
         const sourceUri = this.getSourceUri(action);
-        const taskList = this.loadFromFile(sourceUri, TaskList.is);
-        return taskList.then(resolvedModel => {
-            this.modelState.taskList = resolvedModel;
-        });
-    }
-
-    saveSourceModel(action: SaveModelAction): MaybePromise<void> {
-        const sourceUri = this.getFileUri(action);
-        this.writeFile(sourceUri, this.modelState.taskList);
+        const notations = await this.loadNotationsFromFile(sourceUri, TaskList.is);
+        const sourceModel = await this.lmsClient.getModelById(notations.id);
+        const sModel = this.reconcileNotationsWithSourceModel(notations, sourceModel);
+        // After reconciliation, it is essential to save SModel -- it can get changed after reconciliation
+        this.writeFile(sourceUri, sModel);
+        this.modelState.taskList = sModel;
     }
 
     /*
      * Copied and adapted from @eclipse-glsp abstract-json-model-storage
      */
-    protected override async loadFromFile(sourceUri: string): Promise<unknown>;
-    protected override async loadFromFile<T>(sourceUri: string, guard: TypeGuard<T>): Promise<T>;
-    protected override async loadFromFile<T>(sourceUri: string, guard?: TypeGuard<T>): Promise<T | unknown> {
+    private async loadNotationsFromFile<T>(sourceUri: string, guard: TypeGuard<T>): Promise<T> {
         try {
             const notationPath = this.toPath(sourceUri);
             let fileContent = this.readFile(notationPath);
@@ -45,7 +41,7 @@ export class TaskListStorage extends AbstractJsonModelStorage {
                 const modelId = await this.lmsClient.getModelId(sourceUri);
                 fileContent = this.createModelForEmptyFile(modelId);
             }
-            if (guard && !guard(fileContent)) {
+            if (!guard(fileContent)) {
                 throw new Error('The loaded root object is not of the expected type!');
             }
             return fileContent;
@@ -54,8 +50,33 @@ export class TaskListStorage extends AbstractJsonModelStorage {
         }
     }
 
+    private reconcileNotationsWithSourceModel(notations: TaskList, sourceModel: src.Model): TaskList {
+        const reconciledSModel: TaskList = TaskList.create(notations.id);
+        const unmappedTasksById: Map<string, src.Task> = new Map();
+        sourceModel.tasks.forEach(t => unmappedTasksById.set(t.id, t));
+        for (const nTask of notations.tasks) {
+            const sourceTask = unmappedTasksById.get(nTask.id);
+            if (sourceTask) {
+                unmappedTasksById.delete(nTask.id);
+                reconciledSModel.tasks.push({ ...sourceTask, position: nTask.position, size: nTask.size });
+            }
+        }
+        for (const sourceTask of unmappedTasksById.values()) {
+            reconciledSModel.tasks.push({ ...sourceTask, position: { x: 0, y: 0 } });
+        }
+
+        for (const sourceTransition of sourceModel.transitions) {
+            reconciledSModel.transitions.push({ ...sourceTransition });
+        }
+        return reconciledSModel;
+    }
+
     protected override createModelForEmptyFile(modelId: string): TaskList {
         return TaskList.create(modelId);
     }
+
+    public override saveSourceModel(action: SaveModelAction): MaybePromise<void> {
+        const sourceUri = this.getFileUri(action);
+        this.writeFile(sourceUri, this.modelState.taskList);
     }
 }
